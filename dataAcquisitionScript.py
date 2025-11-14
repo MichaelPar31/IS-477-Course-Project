0f09f5b6b9c0ec5f42a148f369b0729eb03f674d
MichaelPar31 <123028140+MichaelPar31@users.noreply.github.com>
2025-11-14 14:40:42 -0600

    Update and rename Acquisition Script to dataAcquisitionScript.py

---
+++ dataAcquisitionScript.py
@@ -0,0 +1,20 @@
+import requests
+import pandas as pd
+import hashlib
+from pathlib import Path
+import gzip
+import io
+
+csv_salaries_path = "IS-477-Course-Project/NBASalaries.csv"
+csv_stats_path = "IS-477-Course-Project/player_stats.csv"
+
+response = requests.get(csv_salaries_path)
+with open("nba_salaries.csv", "wb") as f:
+    f.write(response.content)
+df_salaries = pd.read_csv("nba_salaries.csv")
+
+response = requests.get(csv_stats_path)
+with open("player_stats.csv", "wb") as f:
+    f.write(response.content)
+df_stats = pd.read_csv("player_stats.csv")
+
